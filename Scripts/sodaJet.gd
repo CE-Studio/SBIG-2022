a322c714ed5cdfc53087d01b155ca862ba332830
clarence112 <clarence112@pop-os.localdomain>
2022-08-07 02:41:45 -0400

    soda works now owo

---
+++ Scripts/sodaJet.gd
@@ -0,0 +1,36 @@
+extends MeshInstance3D
+
+var cbot:Node3D
+var cpart:GPUParticles3D
+var play:Player
+
+var timer := 5.0
+var launch := false
+
+var rng := RandomNumberGenerator.new()
+
+func _ready():
+    cbot = $"Cola"
+    cpart = $"Cola/GPUParticles3D"
+    play = get_parent()
+    rng.randomize()
+    
+func _physics_process(delta):
+    if launch:
+        cbot.position = Vector3(rng.randf_range(-0.1, 0.1), 0.45 + rng.randf_range(-0.1, 0.1), 0.487 + rng.randf_range(-0.1, 0.1))
+        timer -= delta
+        if timer <= 0:
+            cpart.show()
+            play.axis_lock_angular_x = true
+            play.axis_lock_angular_y = true
+            play.axis_lock_angular_z = true
+            play.apply_force(global_transform.basis.y * 100)
+            if timer <= -5:
+                play.axis_lock_angular_x = false
+                play.axis_lock_angular_y = false
+                play.axis_lock_angular_z = false
+                play.remove_child(self)
+        
+func _input(event):
+    if event.is_action_pressed("ui_accept"):
+        launch = true
--- Scripts/sodaPickup.gd
+++ Scripts/sodaPickup.gd
@@ -1,5 +1,6 @@
 extends Area3D
 
+var bottlestrap:Resource = load("res://scenesAndPrefabs/sodaJet.tscn")
 var player:Player
 
 func _ready():
@@ -8,4 +9,9 @@ func _ready():
 
 func body_entered(body:Node3D):
     if body.name == "Player":
-        pass
+        var newbot:Node3D = bottlestrap.instantiate()
+        newbot.set_name("sodaJet")
+        player.add_child(newbot)
+        newbot.position = Vector3.ZERO
+        newbot.rotation = Vector3.ZERO
+        get_parent().remove_child(self)
