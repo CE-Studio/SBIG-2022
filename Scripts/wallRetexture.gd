fdfe651f9e3ec632bad2c4722872aed01660ebf4
clarence112 <clarence112@pop-os.localdomain>
2022-08-06 23:44:14 -0400

    automatic wall retexturing

--- Scripts/wallRetexture.gd
+++ Scripts/wallRetexture.gd
@@ -4,5 +4,6 @@ const _wallid = ["res://textures/wallMat0.tres", "res://textures/wallMat1.tres"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-
+    var h:GameManager = $"/root/GameManager"
+    var i:StandardMaterial3D = load(_wallid[h.current_wall])
+    material_override = i
