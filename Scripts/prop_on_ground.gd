c80b91d5e89f4fd9f372d194242d6d7ca100e677
EpsilonTheDerg <jamesclamey@gmail.com>
2022-08-06 23:02:33 -0400

    no message

--- Scripts/prop_on_ground.gd
+++ Scripts/prop_on_ground.gd
@@ -11,6 +11,6 @@ func _process(delta):
     pass
 
 func body_entered(body:Node3D):
-    if body.name == "Floor" && !_has_hit_floor:
+    if "Floor" in body.name && !_has_hit_floor:
         _has_hit_floor = true
         manager.decrease_sleep(self.name)
