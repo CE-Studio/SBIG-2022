4201aaf3a539b166c4140e89eeedbdd92eda3967
clarence112 <clarencepacelli@gmail.com>
2022-08-12 01:55:20 -0400

    hahaha sfx yes

--- Scripts/prop_on_ground.gd
+++ Scripts/prop_on_ground.gd
@@ -3,9 +3,11 @@ extends RigidDynamicBody3D
 var manager:GameManager
 var _has_hit_floor := false
 @export var myname:String
+var aud:AudioStreamPlayer
 
 func _ready():
     manager = $"/root/GameManager"
+    aud = $"AudioStreamPlayer"
     connect("body_entered", self.body_entered)
 
 #func _process(delta):
@@ -14,4 +16,5 @@ func _ready():
 func body_entered(body:Node3D):
     if ("floor" in body.name) && !_has_hit_floor:
         _has_hit_floor = true
+        aud.play()
         manager.decrease_sleep(myname)
